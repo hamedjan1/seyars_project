72611c92599c9265e4436de9e3bac5e6a1e0af6a
hamedjan1 <hamedazim.123@gmail.com>
2021-04-24 02:01:42 -0400

    first commit

---
+++ src/day01.java
@@ -0,0 +1,148 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+public class day01 {
+
+	public static void main(String[] args) {
+
+
+        
+		
+//		int[][] nums2D= {
+//                {12,  55,  4, 7},    //0
+//                {123, 555, 9},       //1
+//                {56, 87, 455, 34},   //2
+//                {234, 12, 54, 23, 23}//3
+//        };
+//
+//         System.out.println("nums2D[2][2] = " + nums2D[2][2]);
+//
+//        for(int i = 0; i < nums2D.length; i++) {
+//            System.out.println();
+//            for(int j = 0; j < nums2D[i].length; j++) {
+//                System.out.print(nums2D[i][j]);
+//        }
+//        }
+
+		
+		
+		
+		
+		
+		
+		
+//		String original = "race";
+//		
+//		original = original.replace(" ", "");
+//		
+//
+//		String reverse = "";
+//
+//		for(int i = original.length()-1; i >= 0 ; i-- ) {
+//			
+//			reverse = reverse + original.charAt(i);
+//		}
+//		
+//		
+//		boolean palindrom= false;
+//		
+//		for(int i = 0; i <= original.length()-1; i++) {
+//			
+//			if(reverse.charAt(i) == original.charAt(i)) {
+//				palindrom = true;
+//			}
+//			
+//			
+//		}
+//		
+//		if(palindrom) {
+//			System.out.println("palindrom");
+//		}else {
+//			System.out.println("not");
+//		}
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+//        String s = "the game was tied at 2-2";
+//        
+//        
+//        String s2 = s.substring(5);
+//        
+//        
+//        int index1 = s.indexOf("game");
+//        int index2 = s2.indexOf("game");
+//        
+//        System.out.println(index1  + "  "+ index2);
+//      
+//        if(index1 == index2) {
+//        	
+//        	System.out.println(index1);
+//        }else {
+//        	
+//        	System.out.println(index2);
+//        	
+//        }
+        
+        
+		
+		
+//		public static boolean isPalindrome(int integer) {
+//		    int palindrome = integer;
+//		    int reverse = 0;
+//
+//		    // Compute the reverse
+//		    while (palindrome != 0) {
+//		        int remainder = palindrome % 10;
+//		        reverse = reverse * 10 + remainder;
+//		        palindrome = palindrome / 10;
+//		    }
+
+		
+		
+		
+		
+		
+		if(3%9==0) {
+		System.out.println("true");	
+		}
+		
+		
+		
+		
+		
+		
+		
+        
+        
+		
+		
+	}
+	}
+		
+		
+		
+		
+		        
+		        
---
+++ src/day02.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class day02 {
+
+	public static void main(String[] args){
+
+		
+		
+		
+		int arr1[] = {2,53,72,5,6,3,6,84,3};
+		
+
+		ArrayList<Integer>  arr = new ArrayList<>(    );
+		
+		
+		for(int each:  arr1) {
+			
+			arr.add(each);
+				
+		}
+		
+		
+        System.out.println(arr);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
